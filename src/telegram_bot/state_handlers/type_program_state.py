492e32a2bee61985ffa09e2f1461138e025dbd2d
SalvatoreLS <losardosalvatorejr@gmail.com>
2025-04-05 18:26:19 +0200

    Definition of states

--- src/telegram_bot/state_handlers/type_program_state.py
+++ src/telegram_bot/state_handlers/type_program_state.py
@@ -1,6 +1,24 @@
 from base_handler import BaseStateHandler
 
+from state_machine import State
+
 class TypeProgramStateHandler(BaseStateHandler):
     def __init__(self, bot):
-        pass
-    # TODO: Implement the TypeProgramStateHandler class
+        self.update = None
+        self.context = None
+
+        self.next_state = State.TYPE_DAY
+
+    async def handle_message(self, update, context):
+
+        self.update = update
+        self.context = context
+
+        message = update.message
+
+        if self.bot.check_and_set_program(chat_id=self.update.message.chat.id,
+                                program_id=message.text):
+            self.bot.send_message(
+                chat_id=self.update.message.chat.id,
+                text="Selected program: " + self.bot.get_selected_program(chat_id=self.update.message.chat.id) # TODO: Check the functioning of the telegram bot with multiple users
+            )

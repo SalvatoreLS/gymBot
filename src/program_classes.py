aaca0a0515a57efa9542fd9a99f72202a01d82e9
SalvatoreLS <losardosalvatorejr@gmail.com>
2025-04-07 18:06:26 +0200

    started implementation of program classes

--- src/program_classes.py
+++ src/program_classes.py
@@ -1,23 +1,66 @@
 import pandas as pd
 import re
 
+class ExerciseSet:
+    """
+    Class for each set of an exercise
+    """
+    def __init__(self):
+        self.weight = None
+        self.rest = None
+        self.reps = None
+        self.comment = None
+        self.extra_info = None
+    
+    def fill_set(self, weight: float, rest: int, reps: int, extra_info: str = None, comment: str = None):
+        self.weight = weight
+        self.rest = rest
+        self.reps = reps
+        self.extra_info = extra_info
+        self.comment = comment
+
+    def update_weight(self, new_weight: float):
+        self.weight = new_weight
+
+    def update_rest(self, new_rest: int):
+        self.rest = new_rest
+    
+    def update_reps(self, new_reps: int):
+        self.reps = new_reps
+    
+    def update_extra_info(self, new_info: str):
+        self.extra_info = new_info
+    
+    def update_comment(self, comment: str):
+        self.comment = comment
+
+    # TODO: Add functions to get set as formatted string
+
 class Exercise:
     """
     Class for each single exercise
     """
-    # TODO: Implement exercise class
-    pass
+    def __init__(self):
+        self.sets = []
+    
+    # TODO: Add functions to get exercise as formatted string
 
 class DayProgram:
     """
     Class for each day of a program
     """
-    # TODO: Implement day program class
-    pass
+    def __init__(self):
+        self.day_name = None
+        self.exercises = []
+    
+    # TODO: Implement all the other functions and variables
 
 class Program:
     """
     Class for each program
     """
-    # TODO: Implement program class
-    pass
+    def __init__(self):
+        self.program_name = None
+        self.days = []
+
+    # TODO: Implement all the functions
